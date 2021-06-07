5441b95685e3371dc4f9b301f1f462b50e916976
Laurex13 <pritiriti2013@gmail.com>
2021-06-07 15:50:48 +0200

    updated loop structure

--- src/main/java/se/lexicon/laurita/App.java
+++ src/main/java/se/lexicon/laurita/App.java
@@ -7,28 +7,36 @@ import java.util.Scanner;
  *
  */
 public class App {
+    private static final Scanner getInputString = new Scanner(System.in);
+
+
 
     public static void main( String[] args ) {
-        System.out.println("Lets Calculate");
-        System.out.println();
-          Calculator calculator = new Calculator();
-          Scanner scanner = new Scanner(System.in);
-          System.out.println("Enter both Numbers:   a b"  );
-          int a = args.length; //read input
-        int b = args.length; //read input
 
-        a = scanner.nextInt();
-        b = scanner.nextInt();
-        calculator.add (a , b);
-        calculator.multiply(a , b);
-        calculator.subtract(a , b);
-        calculator.divide(a , b);
+        boolean calculate = true;
+        while (calculate) {
+            System.out.println("Do you want to calculate? (y/n)");
+            String result_n = getInputString.nextLine();
 
+            if (result_n.equalsIgnoreCase("n")) {
+                calculate = false;
+            }  else {
 
-        System.out.println("Do you want to perform another calculation? (y/n)");
-          scanner.close();
+                System.out.println("Lets Calculate");
+                System.out.println();
+                Calculator calculator = new Calculator();
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter both Numbers:   a b");
+                int a = args.length; //read input
+                int b = args.length; //read input
 
+                a = scanner.nextInt();
+                b = scanner.nextInt();
+                calculator.add(a, b);
+                calculator.multiply(a, b);
+                calculator.subtract(a, b);
+                calculator.divide(a, b);
+            }
 
-    }
-
-}
+        }
+    }}
